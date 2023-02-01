358bf4dafaa4bdea8666bee52bcb7a21d9bdabb7
Atskyr <124145446+Atskyr@users.noreply.github.com>
2023-02-01 12:50:09 +0200

    Add files via upload

---
+++ LOOP_ETHexebatchprogram.bat
@@ -0,0 +1,5 @@
+:loop
+"C:\Users\atsky\Anaconda3\python.exe" "C:\Users\atsky\Desktop\ETHKyriakosAtsalakis22_algo.py"
+
+timeout /t 900
+goto loop
---
+++ MANTESTLOOP.bat
@@ -0,0 +1,5 @@
+:loop
+"C:\Users\atsky\Anaconda3\python.exe" "C:\Users\atsky\Desktop\manageTEST.py"
+
+timeout /t 60
+goto loop
